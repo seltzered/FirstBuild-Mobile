9da3339d75068efd13137522bcff7a7e6c4f5e43
MylesCaley <myles.caley@gmail.com>
2015-05-18 18:32:57 -0400

    meat sizing screen - constraints

--- MobiUs/FSTBeefSettingsViewController.m
+++ MobiUs/FSTBeefSettingsViewController.m
@@ -7,21 +7,33 @@
 //
 
 #import "FSTBeefSettingsViewController.h"
+#import "FSTBeefSousVideCookingMethod.h"
 
 @interface FSTBeefSettingsViewController ()
 
 @property (strong, nonatomic) IBOutlet UIView *thicknessSelectionView;
+@property (strong, nonatomic) IBOutlet UIView *meatView;
 
 @end
 
 @implementation FSTBeefSettingsViewController
-
-CGFloat startingHeight;
-CGFloat startingOrigin;
+{
+    CGFloat _startingHeight;
+    CGFloat _startingOrigin;
+    CGFloat _maxHeight;
+    CGFloat _meatHeightOffset;
+    CGFloat _yBoundsTop;
+    CGFloat _yBoundsBottom;
+    FSTBeefSousVideCookingMethod* _beefCookingMethod;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    _beefCookingMethod = [[FSTBeefSousVideCookingMethod alloc]init];
+    _maxHeight = self.thicknessSelectionView.frame.size.height;
+    _meatHeightOffset = self.meatView.frame.origin.y;
+    _yBoundsBottom = (self.thicknessSelectionView.frame.origin.y + _maxHeight) - _meatHeightOffset;
+    _yBoundsTop = self.thicknessSelectionView.frame.origin.y;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,24 +46,36 @@ CGFloat startingOrigin;
     
     CGFloat yTranslation =[gesture translationInView:gesture.view.superview].y;
     CGFloat yGestureLocation = [gesture locationInView:gesture.view.superview].y;
-
+    CGFloat newOrigin = _startingOrigin + yTranslation;
+    CGFloat newHeight = _startingHeight - yTranslation;
+    
     if (gesture.state == UIGestureRecognizerStateBegan)
     {
-        startingHeight = self.thicknessSelectionView.frame.size.height;
-        startingOrigin = self.thicknessSelectionView.frame.origin.y;
+        _startingHeight = self.thicknessSelectionView.frame.size.height;
+        _startingOrigin = self.thicknessSelectionView.frame.origin.y;
         
         NSLog(@"start (frame o:%f,s:%f) (bounds o:%f, s:%f)", self.thicknessSelectionView.frame.origin.y, self.thicknessSelectionView.frame.size.height, self.thicknessSelectionView.bounds.origin.y, self.thicknessSelectionView.bounds.size.height);
-        
     }
-    else
+    else if (newOrigin > _yBoundsTop && newOrigin < _yBoundsBottom)
     {
         CGRect frame = self.thicknessSelectionView.frame;
-        frame.origin.y = startingOrigin + yTranslation;
-        frame.size.height = startingHeight - yTranslation;
+        frame.origin.y = newOrigin;
+        frame.size.height = newHeight;
         [self.thicknessSelectionView setFrame:frame];
         
-         NSLog(@"translation: %f, gesture y %f, new y %f, new height %f", yTranslation, yGestureLocation,frame.origin.y, frame.size.height);
+         NSLog(@"translation: %f, gesture y %f, new y %f, new height %f, converted %f", yTranslation, yGestureLocation,frame.origin.y, frame.size.height, [self meatThicknessWithActualViewHeight:frame.size.height]);
     }
+}
+
+- (CGFloat) meatThicknessWithActualViewHeight: (CGFloat)height
+{
+    int index = floor((height-_meatHeightOffset)/(_maxHeight-_meatHeightOffset) * _beefCookingMethod.thicknesses.count);
+    if (index < _beefCookingMethod.thicknesses.count)
+    {
+        NSNumber *thickness = [_beefCookingMethod.thicknesses objectAtIndex:index];
+        return [thickness floatValue];
+    }
+    return 0;
 }
 
 /*
