ada054d35cbb5211741675d0bbb54c448915e0a7
MylesCaley <myles.caley@gmail.com>
2015-05-19 17:01:22 -0400

    icnrease meat view window to bind to bottom; adding fractional thickness labels

--- MobiUs/FSTBeefSettingsViewController.m
+++ MobiUs/FSTBeefSettingsViewController.m
@@ -29,7 +29,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -82,15 +81,5 @@
     }
     return 0;
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
