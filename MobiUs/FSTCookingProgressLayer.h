60fa3f420b4e2eb9e2a775962cc6d68da255fe9c
MylesCaley <myles.caley@gmail.com>
2015-07-23 10:59:05 -0400

    renamed circle progress views/layer to cooking progress

---
+++ MobiUs/FSTCookingProgressLayer.h
@@ -0,0 +1,43 @@
+//
+//  FSTCookingProgressLayer.h
+//  FirstBuild
+//
+//  Created by Myles Caley on 5/22/15.
+//  Copyright (c) 2015 FirstBuild. All rights reserved.
+//
+
+//
+//  CircleShapeLayer.h
+//  CircularProgressControl
+//
+//  Created by Carlos Eduardo Arantes Ferreira on 22/11/14.
+//  Copyright (c) 2014 Mobistart. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+@interface FSTCookingProgressLayer : CAShapeLayer
+
+typedef enum {
+    kPreheating = 0,
+    kReady, // ready to cook
+    kCooking,
+    kSitting
+} ProgressState; // enum to define the progressLayer's states
+
+@property (nonatomic) NSTimeInterval elapsedTime;
+@property (nonatomic) NSTimeInterval timeLimit;
+@property (nonatomic) CGFloat currentTemp;
+@property (assign, nonatomic, readonly) double percent;
+@property (nonatomic) UIColor *progressColor;
+@property (nonatomic) ProgressState layerState;
+@property (nonatomic) CGFloat targetTemp;
+@property (nonatomic) CGFloat startingTemp;
+@end
+
+// Copyright belongs to original author
+// http://code4app.net (en) http://code4app.com (cn)
+// From the most professional code share website: Code4App.net
+
